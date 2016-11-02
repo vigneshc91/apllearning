64783423b60d5dc7758ffb8ed9b04a6bc7115977
Vignesh C <vigneshc@wekancode.com>
2016-11-02 18:50:17 +0530

    active class added for nav bar

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        'user/login', 'user/logout', 'user/changePassword', 'superAdmin/createUser', 'superAdmin/resetPassword', 'superAdmin/deleteUser',
+        'user/login', 'user/logout', 'user/changePassword', 'user/getLoggedInUser', 'superAdmin/createUser', 'superAdmin/resetPassword', 'superAdmin/deleteUser',
         'admin/createGrade', 'admin/editGrade', 'admin/deleteGrade', 'admin/createSubject', 'admin/editSubject', 'admin/deleteSubject',
         'admin/editStudent', 'admin/getUsersList', 'admin/getGradesList', 'admin/getSubjectsList',
         'teacher/createMaterial', 'teacher/editMaterial', 'teacher/deleteMaterial', 'teacher/getMaterialsList',
